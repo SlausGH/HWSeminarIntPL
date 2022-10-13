2b470fa4c87b52260260d64198c7d4a16d12f355
slaus <vmedovb@gmail.com>
2022-10-13 20:26:26 +0300

    PracticalTask01

---
+++ HW1/Ex6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int rem = num1%2;
+if (rem == 0)
+{
+    Console.Write("Число четное");
+}
+else
+{
+    Console.Write("Число НЕ четное");
+}
---
+++ HW1/Ex8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+Console.Write("Четные числа диапозона от 1 до N: ");
+while (count < N-1)
+{
+    Console.Write((count+2)+" ");
+    count = count + 2;
+}
