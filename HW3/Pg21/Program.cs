54323fecab70e091ef9bc46985f5a6f1182b2428
slaus <vmedovb@gmail.com>
2022-11-08 19:51:01 +0300

    PracticalTask03

---
+++ HW3/Pg21/Program.cs
@@ -0,0 +1,20 @@
+﻿// Расстояние между двумя точками в трехмерном прострнстве
+
+Console.WriteLine("Введите координаты точки A ");
+Console.Write("x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B ");
+Console.Write("x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double AB = Math.Sqrt(Math.Pow(x2-x1, 2)+Math.Pow(y2-y1, 2)+Math.Pow(z2-z1, 2));
+Console.WriteLine(AB);
---
+++ HW3/Pg23/Program.cs
@@ -0,0 +1,12 @@
+﻿// Кубы чиисел от 1 до N
+
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= n)
+{
+    double coub = Math.Pow(count, 3);
+    Console.WriteLine($"{count}^3 = {coub}");
+    count++;
+}
