f3e2f65e1a89c57a81ff2503a340786feeaeb1fc
slaus <vmedovb@gmail.com>
2022-11-21 22:29:58 +0300

    after check

---
+++ HW1/Ex0/Program.cs
@@ -0,0 +1,9 @@
+﻿// Степени 2
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+while (count <= n)
+{
+    Console.WriteLine($"2^{count}={Math.Pow(2, count)}");
+    count++;
+}
--- HW3/Pg19/Program.cs
+++ HW3/Pg19/Program.cs
@@ -1,21 +1,21 @@
 ﻿// Является ли 5-ти значное число палиндромом.
 Console.Write("Введите пятизначное число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-int firN = n / 10000;
-int secN = n / 1000 % 10;
-int fouN = n / 10 % 10;
-int fifN = n % 10;
 
 if (n > 9999 && n < 100000)
 {
-    if (firN == fifN)
-    {
-        if (secN == fouN)
+    int firN = n / 10000;
+    int secN = n / 1000 % 10;
+    int fouN = n / 10 % 10;
+    int fifN = n % 10;
+        if (firN == fifN)
         {
-        Console.WriteLine("Yes");
+            if (secN == fouN)
+            {
+            Console.WriteLine("Yes");
+            }
+            else Console.WriteLine("No");
         }
-        else Console.WriteLine("No");
-    }
     else Console.WriteLine("No");
 }
 else Console.WriteLine("It's not a five digit number");
