a6b0cb54c24290d3fba6072e34c536d2fd166d35
slaus <vmedovb@gmail.com>
2022-12-01 21:58:40 +0300

    PracticalTask06

--- HW6/Pg43/Program.cs
+++ HW6/Pg43/Program.cs
@@ -2,15 +2,26 @@
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-Console.WriteLine("Введите k1, b1, k2, b2 попорядку через Entr");
-double[] result = LineCross(EnterNums(), EnterNums(), EnterNums(), EnterNums());
+Console.WriteLine("Введите коэффициенты");
+Console.Write("k1: ");
+double k1 = EnterNums();
+Console.Write("b1: ");
+double b1 = EnterNums();
+Console.Write("k2: ");
+double k2 = EnterNums();
+Console.Write("b2: ");
+double b2 = EnterNums();
+
+double[] result = LineCross(k1, b1, k2, b2);
+Console.Write($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ");
 PrintArray(result);
 
+
 double[] LineCross(double num1, double num2, double num3, double num4)
-{  
+{
     double[] resXY = new double[2];
     resXY[0] = Math.Round((num4 - num2) / (num1 - num3), 2);
-    resXY[1] = Math.Round(num1*resXY[0] + num2, 2);
+    resXY[1] = Math.Round(num1 * resXY[0] + num2, 2);
     return resXY;
 }
 
